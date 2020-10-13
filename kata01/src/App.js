1ec31d26c2360d0ced6c167bee11f92b94adb3d1
Runolf <alexandreliskiewicz@hotmail.com>
2020-10-13 10:04:10 +0200

    move datas in a file

--- kata01/src/App.js
+++ kata01/src/App.js
@@ -1,18 +1,11 @@
 import React from 'react';
 import './App.css';
 import BandList from './Components/BandList/BandList';
+import DATAS from './datas';
 class App extends React.Component{
   
   state = {
-    Bands: [
-      {id:"1", name: "Ye Banished Privateers", style: "pirates"},
-      {id:"2", name: "Limp Bizkit", style: "Neo-Metal"},
-      {id:"3", name: "Wardruna", style: "Neo-folk"},
-      {id:"4", name: "Green Day", style: "Punk Rock"},
-      {id:"5", name: "дора", style: "Russian Pop"},
-      {id:"6", name: "Nightwish", style: "Goth-metal"},
-      {id:"7", name: "Ignea", style: "Oriental-metal"}
-    ]
+    Bands: DATAS
   }
 
   render(){
---
+++ kata01/src/datas.js
@@ -0,0 +1,12 @@
+const BandsDatas = [
+    {id:"1", name: "Ye Banished Privateers", style: "pirates"},
+    {id:"2", name: "Limp Bizkit", style: "Neo-Metal"},
+    {id:"3", name: "Wardruna", style: "Neo-folk"},
+    {id:"4", name: "Green Day", style: "Punk Rock"},
+    {id:"5", name: "дора", style: "Russian Pop"},
+    {id:"6", name: "Nightwish", style: "Goth-metal"},
+    {id:"7", name: "Ignea", style: "Oriental-metal"}
+  ];
+
+
+  export default BandsDatas;
